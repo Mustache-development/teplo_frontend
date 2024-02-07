1119330ee0773294f3c92e0e72095c97328b2396
ItSushko14 <itsushkoandriy@gmail.com>
2024-02-07 16:16:18 +0200

    add page team

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { FC } from "react"
+
+const About: FC = () => {
+  return (
+    <main>
+      <h1>Про нас</h1>
+    </main>
+  )
+}
+
+export default About
---
+++ src/pages/team.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { FC } from "react"
+
+const Team: FC = () => {
+  return (
+    <main>
+      <h1>Команда розробників сайта</h1>
+    </main>
+  )
+}
+
+export default Team
