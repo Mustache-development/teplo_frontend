6f3478349450b2e221de04e64073e80b9ef41654
ItSushko14 <itsushkoandriy@gmail.com>
2024-09-15 18:59:36 +0300

    renew loginUI

--- src/admin/components/Login/LoginUI.tsx
+++ src/admin/components/Login/LoginUI.tsx
@@ -1,41 +1,37 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Snackbar from '@mui/material/Snackbar';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Snackbar from "@mui/material/Snackbar";
 
-import { 
-  IconButton, 
-  InputAdornment, 
+import {
+  IconButton,
+  InputAdornment,
   FormControl,
   InputLabel,
   OutlinedInput,
-  List, 
-  ListItem, 
-  ListItemText, 
+  List,
+  ListItem,
+  ListItemText,
   CircularProgress,
-  Alert
-} from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-
+  Alert,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Тепло на передову
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
+      {"Copyright © "}
+      Тепло на передову {new Date().getFullYear()}
+      {"."}
     </Typography>
   );
 }
@@ -45,31 +41,24 @@ const defaultTheme = createTheme();
 interface LoginFormProps {
   handlesubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   isLoading: boolean;
-  snackbarData: {open: boolean; text: string; isSuccess: boolean};
-  setSnackbarData: (data:{ open: boolean; text: string; isSuccess: boolean }) => void;
+  snackbarData: { open: boolean; text: string; isSuccess: boolean };
+  setSnackbarData: (data: { open: boolean; text: string; isSuccess: boolean }) => void;
 }
 
-const LoginUI: React.FC<LoginFormProps> = ({
-                                            handleSubmit,
-                                            isLoading,
-                                            snackbarData,
-                                            setSnackbarData
-                                          }) => {
-  
-
+const LoginUI: React.FC<LoginFormProps> = ({ handleSubmit, isLoading, snackbarData, setSnackbarData }) => {
   const [showPassword, setShowPassword] = React.useState(false);
-  
+
   const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
-    return;
+    if (reason === "clickaway") {
+      return;
     }
-    setSnackbarData({...snackbarData, open:false})
-}
+    setSnackbarData({ ...snackbarData, open: false });
+  };
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
-    };
-  
+  };
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
@@ -77,19 +66,18 @@ const LoginUI: React.FC<LoginFormProps> = ({
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Вхід в адміністративну панель
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          
             <TextField
               margin="normal"
               required
@@ -99,7 +87,6 @@ const LoginUI: React.FC<LoginFormProps> = ({
               name="email"
               autoComplete="email"
               autoFocus
-
             />
             <TextField
               margin="normal"
@@ -108,28 +95,21 @@ const LoginUI: React.FC<LoginFormProps> = ({
               variant="outlined"
               name="password"
               label="Пароль"
-              type={showPassword ? 'text' : 'password'}
+              type={showPassword ? "text" : "password"}
               id="password"
               autoComplete="current-password"
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">
                     <IconButton onClick={handleClickShowPassword}>
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
                     </IconButton>
                   </InputAdornment>
                 ),
               }}
-              
             />
-            
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              disabled={isLoading}
-            >
+
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} disabled={isLoading}>
               Увійти
             </Button>
             <Grid container>
@@ -138,27 +118,23 @@ const LoginUI: React.FC<LoginFormProps> = ({
                   Забули пароль?
                 </Link>
               </Grid>
-              
             </Grid>
           </Box>
         </Box>
         <Snackbar
-            open={snackbarData.open}
-            onClose={handleClose}
-            anchorOrigin={{ vertical:"top", horizontal:"center"}}
-            autoHideDuration={3000}
+          open={snackbarData.open}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          autoHideDuration={3000}
         >
-            <Alert
-                onClose={handleClose}
-                severity={snackbarData.isSuccess ? "success" : "error"}
-            >
-                {snackbarData.text}
-            </Alert>
+          <Alert onClose={handleClose} severity={snackbarData.isSuccess ? "success" : "error"}>
+            {snackbarData.text}
+          </Alert>
         </Snackbar>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
-}
+};
 
-export default LoginUI
+export default LoginUI;
