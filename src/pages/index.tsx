c41867e969b07888a9f2cc65d66b7b4ba41b942c
ItSushko14 <itsushkoandriy@gmail.com>
2024-09-24 18:03:22 +0300

    comment detailed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import AboutProject from "../components/AboutProject/AboutProject";
 // import Stat from "../components/Stat/Stat";
 import HowWeToWork from "../components/HowWeToWork/HowWeToWork";
 import Telegram from "../components/Telegram/Telegram";
-import Detailed from "../components/Detailed/Detailed";
+// import Detailed from "../components/Detailed/Detailed";
 import Position from "../components/Position/Position";
 import OurTeam from "../components/OurTeam/OurTeam";
 import Monobank from "../components/Monobank/Monobank";
@@ -25,7 +25,7 @@ const IndexPage: React.FC<PageProps> = () => {
       <HowWeToWork />
       <Telegram />
       <Monobank />
-      <Detailed />
+      {/* <Detailed /> */}
       <Position />
       <OurTeam />
       {/* <Donators /> */}
