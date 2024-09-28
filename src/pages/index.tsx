846895ee22941e96cbdee2285604c6b5cc04a4d3
ItSushko14 <itsushkoandriy@gmail.com>
2024-09-28 17:12:37 +0300

    uncomment stat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "gatsby";
 import GlobalStyle from "../styles/GlobalStyle";
 import Header from "../components/Header/Header";
 import AboutProject from "../components/AboutProject/AboutProject";
-// import Stat from "../components/Stat/Stat";
+import Stat from "../components/Stat/Stat";
 import HowWeToWork from "../components/HowWeToWork/HowWeToWork";
 import Telegram from "../components/Telegram/Telegram";
 // import Detailed from "../components/Detailed/Detailed";
@@ -21,7 +21,7 @@ const IndexPage: React.FC<PageProps> = () => {
       <GlobalStyle />
       <Header />
       <AboutProject />
-      {/* <Stat /> */}
+      <Stat />
       <HowWeToWork />
       <Telegram />
       <Monobank />
