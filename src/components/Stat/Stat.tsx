5db14941d8156d8f0fdb4a110ab870c64501ea7b
Maxim Rudaites <rudajtes@gmail.com>
2024-09-30 09:41:12 +0300

    Partial PC adaptation(AboutProject, Header, Hero, Stat)

--- src/components/Stat/Stat.tsx
+++ src/components/Stat/Stat.tsx
@@ -7,18 +7,20 @@ const Stat: React.FC<StatProps> = ({}) => {
   return (
     <section className={styles.container} id="stat">
       <div className="title darkColor">Наші досягнення</div>
-      <div className={`${styles.block} ${styles.background1}`}>
-        <div className={styles.number}>300+</div>
-        <div className={styles.name}>Закритих запитів</div>
-      </div>
-      <div className={`${styles.block} ${styles.background2}`}>
-        <div className={styles.number}>23</div>
-        <div className={styles.name}>Військових підрозділів</div>
-      </div>
-      <div className={`${styles.block} ${styles.background3}`}>
-        <div className={styles.number}>40+</div>
-        <div className={styles.name}>Постійних донатерів</div>
-      </div>
+      <div className={styles.rowContainer}>
+        <div className={`${styles.block} ${styles.background1}`}>
+          <div className={styles.number}>300+</div>
+          <div className={styles.name}>Закритих запитів</div>
+        </div>
+        <div className={`${styles.block} ${styles.background2}`}>
+          <div className={styles.number}>23</div>
+          <div className={styles.name}>Військових підрозділів</div>
+        </div>
+        <div className={`${styles.block} ${styles.background3}`}>
+          <div className={styles.number}>40+</div>
+          <div className={styles.name}>Постійних донатерів</div>
+        </div>
+      </div> 
     </section>
   );
 };
